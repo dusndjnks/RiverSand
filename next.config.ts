9cebdf0e3e0e75444a5ad24637757bd5a259b7fa
dusndjnks <mhdaffan0000@gmail.com>
2025-08-18 02:58:08 +0530

    x

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["example.com", "cdn.mywebsite.com"], // add more if needed
+    domains: ["example.com", "cdn.mywebsite.com"], // add more domains if needed
   },
-  output: "export", // ✅ replaces next export
 };
 
 export default nextConfig;
